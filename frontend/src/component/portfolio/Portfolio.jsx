77f2d8c501c6f3359b058dd8e27d7950d05c7b62
Dilawar9 <56985882+Dilawar9@users.noreply.github.com>
2024-02-17 14:13:33 +0500

    chang

--- frontend/src/component/portfolio/Portfolio.jsx
+++ frontend/src/component/portfolio/Portfolio.jsx
@@ -16,7 +16,34 @@ function Portfolio() {
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000
+    autoplaySpeed: 2000,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
 
   const img = [
